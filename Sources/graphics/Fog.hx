8e8e367eba71b1817a87c3ebed006a3a99f2b58c
John Doughty <jrdoughty13@gmail.com>
2017-05-28 10:59:52 -0500

    feathered the edges!

--- Sources/graphics/Fog.hx
+++ Sources/graphics/Fog.hx
@@ -56,86 +56,107 @@ class Fog extends Tilemap
 
 	private function drawArea(r:Int, c:Int, canvas:Canvas, objectX:Float, objectY:Float, cameraX:Float, cameraY:Float)
 	{
-		var tile = map[r][c];
-		if (tile != -1)
-		{
-			_px = objectX + x + (c * tileset.tileWidth) - cameraX;
-			_py = objectY + y + (r * tileset.tileHeight) - cameraY;
-			
-			var config:Int = 0;
-			if(map[r][c] == 0) config += TOP_LEFT;
-			if(c < map[r].length && map[r][c+1] == 0) config += TOP_RIGHT;
-			if(r < map.length && c < map[r].length && map[r+1][c+1] == 0) config += BOTTOM_RIGHT;
-			if(r < map.length && map[r+1][c] == 0) config += BOTTOM_LEFT;
-			
-			canvas.g2.color = kha.Color.Black;
-			
-			switch config {
-				case 0: 
-					canvas.g2.opacity = .5;
-					canvas.g2.fillRect(_px, _py, tileset.tileWidth, tileset.tileHeight);
-				case 1:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px + tileset.tileWidth / 2, _py, tileset.tileWidth / 2, tileset.tileHeight / 2);
-					canvas.g2.fillRect(_px, _py + tileset.tileHeight / 2, tileset.tileWidth / 2, tileset.tileHeight / 2);
-					canvas.g2.opacity = .5;
-					canvas.g2.fillRect(_px + tileset.tileWidth / 2, _py + tileset.tileHeight / 2, tileset.tileWidth / 2, tileset.tileHeight / 2);
-				case 2:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px + tileset.tileWidth / 2, _py + tileset.tileHeight / 2, tileset.tileWidth / 2, tileset.tileHeight / 2);
-					canvas.g2.fillRect(_px, _py, tileset.tileWidth / 2, tileset.tileHeight / 2);
-					canvas.g2.opacity = .5;
-					canvas.g2.fillRect(_px, _py + tileset.tileHeight / 2, tileset.tileWidth / 2, tileset.tileHeight / 2);
-				case 3://TOP_LEFT | TOP_RIGHT:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px, _py + tileset.tileHeight / 2, tileset.tileWidth, tileset.tileHeight / 2);
-				case 4:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px + tileset.tileWidth / 2, _py + tileset.tileHeight / 2, tileset.tileWidth / 2, tileset.tileHeight / 2);
-					canvas.g2.fillRect(_px, _py, tileset.tileWidth / 2, tileset.tileHeight / 2);
-					canvas.g2.opacity = .5;
-					canvas.g2.fillRect(_px + tileset.tileWidth / 2, _py, tileset.tileWidth / 2, tileset.tileHeight / 2);
-				case 5://TOP_LEFT | BOTTOM_LEFT:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px + tileset.tileWidth / 2, _py, tileset.tileWidth / 2, tileset.tileHeight);
-				case 6:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px, _py, tileset.tileWidth / 2, tileset.tileHeight / 2);
-					canvas.g2.fillRect(_px + tileset.tileWidth / 2, _py + tileset.tileHeight / 2, tileset.tileWidth / 2, tileset.tileHeight / 2);
-				case 7://TOP_LEFT | TOP_RIGHT | BOTTOM_LEFT:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px + tileset.tileWidth / 2, _py + tileset.tileHeight / 2, tileset.tileWidth / 2, tileset.tileHeight / 2);
-				case 8:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px + tileset.tileWidth / 2, _py, tileset.tileWidth / 2, tileset.tileHeight / 2);
-					canvas.g2.fillRect(_px, _py + tileset.tileHeight / 2, tileset.tileWidth / 2, tileset.tileHeight / 2);
-					canvas.g2.opacity = .5;
-					canvas.g2.fillRect(_px, _py, tileset.tileWidth / 2, tileset.tileHeight / 2);
-				case 9://TOP_LEFT | BOTTOM_RIGHT:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px + tileset.tileWidth / 2, _py, tileset.tileWidth / 2, tileset.tileHeight / 2);
-					canvas.g2.fillRect(_px, _py + tileset.tileHeight / 2, tileset.tileWidth / 2, tileset.tileHeight / 2);
-				case 10://TOP_RIGHT | BOTTOM_RIGHT:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px, _py, tileset.tileWidth / 2, tileset.tileHeight);
-				case 11://TOP_LEFT | TOP_RIGHT | BOTTOM_RIGHT:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px, _py + tileset.tileHeight / 2, tileset.tileWidth / 2, tileset.tileHeight / 2);
-				case 12://BOTTOM_LEFT | BOTTOM_RIGHT:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px, _py, tileset.tileWidth, tileset.tileHeight / 2);
-				case 13://TOP_LEFT | BOTTOM_LEFT | BOTTOM_RIGHT:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px + tileset.tileWidth / 2, _py, tileset.tileWidth / 2, tileset.tileHeight / 2);
-				case 14://TOP_RIGHT | BOTTOM_LEFT | BOTTOM_RIGHT:
-					canvas.g2.opacity = .25;
-					canvas.g2.fillRect(_px, _py, tileset.tileWidth / 2, tileset.tileHeight / 2);
-				default:
-			}
-
-
+		var fullTW = tileset.tileWidth;
+		var halfTW = fullTW/2;
+		var quarTW = fullTW/4;
+		var fullTH = tileset.tileHeight;
+		var halfTH = fullTH/2;
+		var quarTH = fullTH/4;
+		_px = objectX + x + (c * fullTW) - cameraX;
+		_py = objectY + y + (r * fullTH) - cameraY;
+		
+		var config:Int = 0;
+		if(map[r][c] == 0) config += TOP_LEFT;
+		if(c < map[r].length && map[r][c+1] == 0) config += TOP_RIGHT;
+		if(r < map.length && c < map[r].length && map[r+1][c+1] == 0) config += BOTTOM_RIGHT;
+		if(r < map.length && map[r+1][c] == 0) config += BOTTOM_LEFT;
+		
+		canvas.g2.color = kha.Color.Black;
+		
+		switch config {
+			case 0: 
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px, _py, fullTW, fullTH);
+			case 1:
+				canvas.g2.opacity = .25;
+				canvas.g2.fillRect(_px + quarTW, _py, quarTW, quarTH);
+				canvas.g2.fillRect(_px, _py + quarTH, quarTW, quarTH);
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px + quarTW, _py + quarTH, quarTW, quarTH);
+				canvas.g2.fillRect(_px + halfTW, _py, halfTW, fullTH);
+				canvas.g2.fillRect(_px, _py + halfTW, halfTW, halfTH);
+			case 2:
+				canvas.g2.opacity = .25;
+				canvas.g2.fillRect(_px + quarTW + halfTW, _py + quarTH, quarTW, quarTH);
+				canvas.g2.fillRect(_px + halfTW, _py, quarTW, quarTH);
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px + halfTW, _py + quarTH, quarTW, quarTH);
+				canvas.g2.fillRect(_px, _py, halfTW, fullTH);
+				canvas.g2.fillRect(_px + halfTW, _py + halfTW, halfTW, halfTH);
+			case 3://TOP_LEFT | TOP_RIGHT:
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px, _py + halfTH, fullTW, halfTH);
+			case 4:
+				canvas.g2.opacity = .25;
+				canvas.g2.fillRect(_px + quarTW, _py + quarTH + halfTH, quarTW, quarTH);
+				canvas.g2.fillRect(_px, _py + halfTH, quarTW, quarTH);
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px + quarTW, _py + halfTH, quarTW, quarTH);
+				canvas.g2.fillRect(_px + halfTW, _py, halfTW, fullTH);
+				canvas.g2.fillRect(_px, _py, halfTW, halfTH);
+			case 5://TOP_LEFT | BOTTOM_LEFT:
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px + halfTW, _py, halfTW, fullTH);
+			case 6:
+				canvas.g2.opacity = .25;
+				canvas.g2.fillRect(_px, _py, halfTW, halfTH);
+				canvas.g2.fillRect(_px + halfTW, _py + halfTH, halfTW, halfTH);
+			case 7://TOP_LEFT | TOP_RIGHT | BOTTOM_LEFT:
+				canvas.g2.opacity = .25;
+				canvas.g2.fillRect(_px + halfTW + quarTW, _py + halfTH, quarTW, quarTH);
+				canvas.g2.fillRect(_px + halfTW, _py + halfTH + quarTH, quarTW, quarTH);
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px + halfTW + quarTW, _py + halfTH + quarTH, quarTW, quarTH);
+			case 8:
+				canvas.g2.opacity = .25;
+				canvas.g2.fillRect(_px + quarTW + halfTW, _py + halfTH, quarTW, quarTH);
+				canvas.g2.fillRect(_px + halfTW, _py + quarTH + halfTH, quarTW, quarTH);
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px + halfTW, _py + halfTH, quarTW, quarTH);
+				canvas.g2.fillRect(_px, _py, halfTW, fullTH);
+				canvas.g2.fillRect(_px + halfTW, _py, halfTW, halfTH);
+			case 9://TOP_LEFT | BOTTOM_RIGHT:
+				canvas.g2.opacity = .25;
+				canvas.g2.fillRect(_px + halfTW, _py, halfTW, halfTH);
+				canvas.g2.fillRect(_px, _py + halfTH, halfTW, halfTH);
+			case 10://TOP_RIGHT | BOTTOM_RIGHT:
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px, _py, halfTW, fullTH);
+			case 11://TOP_LEFT | TOP_RIGHT | BOTTOM_RIGHT:
+				canvas.g2.opacity = .25;
+				canvas.g2.fillRect(_px + quarTW, _py + quarTH + halfTH, quarTW, quarTH);
+				canvas.g2.fillRect(_px, _py + halfTH, quarTW, quarTH);
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px, _py + quarTH + halfTH, quarTW, quarTH);
+			case 12://BOTTOM_LEFT | BOTTOM_RIGHT:
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px, _py, fullTW, halfTH);
+			case 13://TOP_LEFT | BOTTOM_LEFT | BOTTOM_RIGHT:
+				canvas.g2.opacity = .25;
+				canvas.g2.fillRect(_px + quarTW + halfTW, _py + quarTH, quarTW, quarTH);
+				canvas.g2.fillRect(_px + halfTW, _py, quarTW, quarTH);
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px + quarTW + halfTW, _py, quarTW, quarTH);
+			case 14://TOP_RIGHT | BOTTOM_LEFT | BOTTOM_RIGHT:
+				canvas.g2.opacity = .25;
+				canvas.g2.fillRect(_px + quarTW, _py, quarTW, quarTH);
+				canvas.g2.fillRect(_px, _py + quarTH, quarTW, quarTH);
+				canvas.g2.opacity = .5;
+				canvas.g2.fillRect(_px, _py, quarTW, quarTH);
+			default:
 		}
 	}
+
 	private function drawbasic(canvas:kha.Canvas, _px:Float, _py:Float)
 	{
 		canvas.g2.color = kha.Color.Black;
