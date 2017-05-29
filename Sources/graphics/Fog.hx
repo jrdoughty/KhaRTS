a35308c8c5d05ad8279a58b835c1539c58d2a20a
John Doughty <jrdoughty13@gmail.com>
2017-05-29 09:42:33 -0500

    Created Fog Buffer

--- Sources/graphics/Fog.hx
+++ Sources/graphics/Fog.hx
@@ -10,6 +10,7 @@ import sdg.atlas.Region;
 import sdg.Graphic.ImageType;
 using kha.graphics2.GraphicsExtension;
 import sdg.graphics.tiles.Tilemap;
+import sdg.graphics.tiles.Tileset;
 import sdg.Sdg;
 
 class Fog extends Tilemap
@@ -18,6 +19,15 @@ class Fog extends Tilemap
 	private static inline var TOP_RIGHT = 2;
 	private static inline var BOTTOM_LEFT = 4;
 	private static inline var BOTTOM_RIGHT = 8;
+
+	private var bBuffer:Image;
+
+	public function new(tileset:Tileset)
+	{
+		super(tileset);
+		bBuffer= Image.createRenderTarget(Sdg.screen.camera.width, Sdg.screen.camera.height);
+	}
+
 	override function render(canvas:Canvas, objectX:Float, objectY:Float, cameraX:Float, cameraY:Float):Void 
 	{
 		canvas.g2.color = color;
@@ -36,14 +46,19 @@ class Fog extends Tilemap
 			
 		if (_endRow < heightInTiles)
 			_endRow++;
-
+		canvas.g2.end();
+		bBuffer.g2.begin(true, kha.Color.Transparent);
 		for (r in _startRow...(_endRow))
 		{
 			for (c in _startCol...(_endCol))
 			{
-				drawArea(r, c, canvas, objectX, objectY, cameraX, cameraY);
+				drawArea(r, c, bBuffer, objectX, objectY, cameraX, cameraY);
 			}
 		}
+		
+		bBuffer.g2.end();
+		canvas.g2.begin(false);
+		canvas.g2.drawImage(bBuffer,0, 0);
 		canvas.g2.opacity = 1;
 	}
 
